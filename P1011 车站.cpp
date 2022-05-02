dbc22b849500d8f5f8d57a95f3f014cdea57146c
MachineryZ <zhizuozhang@gmail.com>
2022-05-02 19:02:58 +0800

    add p1141

---
+++ P1011 车站.cpp
@@ -0,0 +1,24 @@
+#include <cstring>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <vector>
+#include <queue>
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+#include<bits/stdc++.h>
+using namespace std;
+
+int a;
+int n;
+int m;
+int x;
+
+int main()
+{
+    cin >> a >> n >> m >> x;
+    vector()
+    return 0;
+}
--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Luogu Practice https://www.luogu.com.cn/
 + P1030 求先序排列: [普及-][NOIp 普及组][2001][recursion]
 + P1036 选数: [普及-][NOIp 普及组][2002][depth_first_search]
 + P1123 取数游戏: [普及/提高-][depth_first_search][recursion]
++ P1141 01迷宫：[普及-][depth_first_search]
 + P1331 海战: [普及-][福建省历届夏令营][depth_first_search]
 + P1451 求细胞数量: [普及-][depth_first_search]
 + P1498 南蛮图腾: [普及-][math]
